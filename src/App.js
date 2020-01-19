6f18807f367ff8016160b6a9f1ad438e0b5dfc95
unknown <ariezncahyo@gmail.com>
2020-01-19 22:55:55 +0700

    Initial react bulma starter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App () {
+    return(
+        <div>
+            <h1>Hello..</h1>
+            <p>We are start with react and bulma</p>
+        </div>
+    )
+}
+
+export default App;
